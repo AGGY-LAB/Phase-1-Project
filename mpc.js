738b7537e2689ccf36d7c6abd78e4de1b4fba012
Aggy <muneeaggy@gmail.com>
2024-08-01 13:29:56 +0300

    My third commit

--- mpc.js
+++ mpc.js
@@ -9,18 +9,41 @@ function handleAllInputs(){
         /// save the input information
         currentAge= event.target.parentNode.querySelector("#age").value
         retireAge= event.target.parentNode.querySelector("#retAge").value
-        rate= event.target.parentNode.querySelector("#interestRate").value
+        rate= (event.target.parentNode.querySelector("#interestRate").value)/100
         monthlyPension= event.target.parentNode.querySelector("#pension_pm").value
 
         // Input Manipulation
         totalSavings = monthlyPension*12*annuityRate
-        console.log(totalSavings)
+        // console.log(totalSavings)
+        timeToRetirement = retireAge - currentAge
+        intFactor = (1+rate)**(timeToRetirement)
+        accumFactor = (intFactor-1)/rate
+        monthlySavings= Math.round(totalSavings/(12*accumFactor))
 
-        let p = document.createElement("p")
-        p.textContent = `For you to have a monthly pension of KShs ${monthlyPension} when you retire at age ${retireAge} years, you will need to have total amount saved of : K Shs ${totalSavings}`
+
+
+
+        let p1 = document.createElement("p")
+        let p2 = document.createElement("p")
+
+        p1.textContent = `Your desired pension after retirement is KShs ${monthlyPension.toLocaleString()} per month. \n
+        To achieve this,  you will need to have saved a total amount of : K Shs ${totalSavings.toLocaleString()}.`
+
+        p2.textContent = `Assuming you are starting to save at your current age, you will need to save of KShs ${monthlySavings.toLocaleString()} per month from now to age ${retireAge}.`
+
+
+
 
         let resultsOutput = document.querySelector(".results")
-        resultsOutput.appendChild(p)
+        resultsOutput.appendChild(p1)
+        resultsOutput.appendChild(p2)
+
+
+        /// Style the output.
+
+        p.style.textAlign="justify"
+
+
 
 
 
